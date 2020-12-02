5d9e6f70265a1dfca065af251d833e68be79106f
nikoloz kvaratskhelia <nkvar12@gmail.com>
2020-12-02 06:52:21 +0200

    - moved app component to component/app directory - removed default content in app component - formatting

--- projects/universe-simulator-app/src/app/app.module.ts
+++ projects/universe-simulator-app/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+import { AppComponent } from './component/app/app.component';
 
 @NgModule({
   declarations: [
@@ -15,4 +16,4 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
